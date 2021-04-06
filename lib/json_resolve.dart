92c60d281d91fdc26e6357a3054d53dd10c258f5
Thomas <samoht.t@hotmail.fr>
2021-04-06 19:35:14 +0200

    Made defaultValue not required
    + Added documentation
    + Updated tests

--- lib/json_resolve.dart
+++ lib/json_resolve.dart
@@ -1,12 +1,20 @@
-///
-/// json_resolve
-/// Created by Khoa Pham
-///
+//
+// json_resolve
+// Created by Khoa Pham
+// Updated by SpooKy12
+//
 
-T resolve<T>({
+/// Resolve nested JSON property from provided [json] into
+/// [T?] object using [path].
+///
+/// If [defaultValue] is provided, it will return it if [path]
+/// doesn't exist in [json] or an error is raised.
+/// With a [defaultValue] set to non-null value, you can use the
+/// bang operator (!) to cast the return value back to [T].
+T? resolve<T>({
   required Map<String, dynamic>? json,
   required String path,
-  required T defaultValue,
+  T? defaultValue,
 }) {
   try {
     if (json == null) {
--- test/json_resolve_test.dart
+++ test/json_resolve_test.dart
@@ -1,7 +1,8 @@
-///
-/// json_resolve
-/// Created by Khoa Pham
-///
+//
+// json_resolve
+// Created by Khoa Pham
+// Updated by SpooKy12
+//
 
 import 'package:test/test.dart';
 import 'package:json_resolve/json_resolve.dart';
@@ -11,59 +12,64 @@ void main() {
     "movie": "isFun",
     "earth": 199999,
     "dc": [
-      {
-        "name": "Superman"
-      },
-      {
-        "name": "Flash"
-      },
-      {
-        "name": "Wonder Woman"
-      }
+      {"name": "Superman"},
+      {"name": "Flash"},
+      {"name": "Wonder Woman"}
     ],
     "marvel": [
       {
         "name": "Thor",
         "weapon": "Mjolnir",
         "appear": [
-          {
-            "year": 2011,
-            "title": "Thor"
-          },
-          {
-            "year": 2013,
-            "title": "The Dark World"
-          },
-          {
-            "year": 2017,
-            "title": "Ragnarok"
-          }
+          {"year": 2011, "title": "Thor"},
+          {"year": 2013, "title": "The Dark World"},
+          {"year": 2017, "title": "Ragnarok"}
         ]
       }
     ]
   };
 
   test('resolve', () {
-    final String byProperty = resolve(json: json, path: "movie", defaultValue: "error");
+    final String byProperty =
+        resolve(json: json, path: "movie", defaultValue: "error")!;
+    final String? byPropertyNullable = resolve(json: json, path: "movie");
     expect(byProperty, "isFun");
+    expect(byPropertyNullable, "isFun");
 
-    final int byInt = resolve(json: json, path: "earth", defaultValue: 0);
+    final int byInt = resolve(json: json, path: "earth", defaultValue: 0)!;
+    final int? byIntNullable =
+        resolve(json: json, path: "earth", defaultValue: 0);
     expect(byInt, 199999);
+    expect(byIntNullable, 199999);
 
-    final String byIndex = resolve(json: json, path: "dc.2.name", defaultValue: "error");
+    final String byIndex =
+        resolve(json: json, path: "dc.2.name", defaultValue: "error")!;
+    final String? byIndexNullable = resolve(json: json, path: "dc.2.name");
     expect(byIndex, "Wonder Woman");
+    expect(byIndexNullable, "Wonder Woman");
 
-    final String byIndexThenProperty = resolve(json: json, path: "marvel.0.appear.1.title", defaultValue: "error");
+    final String byIndexThenProperty = resolve(
+        json: json, path: "marvel.0.appear.1.title", defaultValue: "error")!;
+    final String? byIndexThenPropertyNullable =
+        resolve(json: json, path: "marvel.0.appear.1.title");
     expect(byIndexThenProperty, "The Dark World");
+    expect(byIndexThenPropertyNullable, "The Dark World");
   });
 
   test("wrong type", () {
-    final int byInt = resolve(json: json, path: "marvel.0.name", defaultValue: 999);
+    final int byInt =
+        resolve(json: json, path: "marvel.0.name", defaultValue: 999)!;
+    final int? byIntNullable = resolve(json: json, path: "marvel.0.name");
     expect(byInt, 999);
+    expect(byIntNullable, null);
   });
 
   test("not found", () {
-    final String byString = resolve(json: json, path: "marvel.0.skill", defaultValue: "error");
+    final String byString =
+        resolve(json: json, path: "marvel.0.skill", defaultValue: "error")!;
+    final String? byStringNullable =
+        resolve(json: json, path: "marvel.0.skill");
     expect(byString, "error");
+    expect(byStringNullable, null);
   });
 }
